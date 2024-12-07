aaa59142b10f095c19c194c0177eb0deeb83be8e
Kaif Ali <kaifali@cmail.carleton.ca>
2024-12-06 19:17:06 -0500

    custom premium tiles

---
+++ src/BoardConfigParser.java
@@ -0,0 +1,31 @@
+package src;
+
+import GUI.ScrabbleController;
+import org.json.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardConfigParser {
+    public static List<PremiumSquare> loadFromJSON(String filePath) {
+        List<PremiumSquare> premiumSquares = new ArrayList<>();
+        try {
+            String content = new String(Files.readAllBytes(Paths.get(filePath)));
+            JSONObject boardConfig = new JSONObject(content);
+            JSONArray squaresArray = boardConfig.getJSONArray("premiumSquares");
+
+            for (int i = 0; i < squaresArray.length(); i++) {
+                JSONObject square = squaresArray.getJSONObject(i);
+                int row = square.getInt("row");
+                int col = square.getInt("col");
+                String type = square.getString("type");
+
+                premiumSquares.add(new PremiumSquare(row, col, type));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return premiumSquares;
+    }
+}
---
+++ src/PremiumSquare.java
@@ -0,0 +1,23 @@
+public class PremiumSquare {
+    private int row;
+    private int col;
+    private String type;
+
+    public PremiumSquare(int row, int col, String type) {
+        this.row = row;
+        this.col = col;
+        this.type = type;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
